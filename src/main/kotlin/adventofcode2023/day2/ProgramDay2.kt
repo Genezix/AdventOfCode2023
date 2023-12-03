d728dce5c3512385bf07bb64aeb31a6f8b763b4f
Florian KAISER <florian.kaiser@helios.do>
2023-12-03 16:27:08 +0100

    day2

--- src/main/kotlin/adventofcode2023/day2/ProgramDay2.kt
+++ src/main/kotlin/adventofcode2023/day2/ProgramDay2.kt
@@ -3,9 +3,76 @@ package adventofcode2023.day2
 import common.Program
 
 class ProgramDay2(brutInputs: List<String>, private val debug: Boolean = false) : Program {
-    private val inputs = brutInputs
+    private val inputs = brutInputs.map {
+        Game.build(it)
+    }
 
-    override fun part1(): String = ""
+    override fun part1(): String {
 
-    override fun part2(): String = ""
+        val valGames = inputs.filter {
+            it.cubeGrabs.all {
+                it.all {
+                    it.nb <= it.color.max
+                }
+            }
+        }
+
+        return valGames.sumOf { it.id }.toString()
+    }
+
+    override fun part2(): String {
+
+        val multiplies = inputs.map {
+            var maxRed = 0
+            var maxGreen = 0
+            var maxBlue = 0
+            it.cubeGrabs.forEach {
+                it.forEach {
+                    when(it.color) {
+                        CubeColor.RED -> maxRed = if(maxRed < it.nb) it.nb else maxRed
+                        CubeColor.GREEN -> maxGreen = if(maxGreen < it.nb) it.nb else maxGreen
+                        CubeColor.BLUE -> maxBlue = if(maxBlue < it.nb) it.nb else maxBlue
+                    }
+                }
+            }
+            maxRed * maxGreen * maxBlue
+        }
+
+        return multiplies.sum().toString()
+    }
+}
+
+data class Game(val id: Int, val cubeGrabs: List<List<NbCube>>) {
+    companion object {
+        val idRegex = "Game ([0-9]*): .*".toRegex()
+        val cubesRegex = "Game [0-9]*: (.*)".toRegex()
+        fun build(input: String): Game {
+            val id = idRegex.find(input)!!.groupValues[1].toInt()
+            val cubeGrabs = cubesRegex.find(input)!!.groupValues[1]
+            val grabs = cubeGrabs.split(";")
+                .map {
+                    it
+                        .trim()
+                        .split(",")
+                        .map {
+                            it
+                                .trim()
+                                .split(" ").let {
+                                    NbCube(
+                                        nb = it[0].toInt(),
+                                        color = CubeColor.valueOf(it[1].uppercase())
+                                    )
+                                }
+                        }
+
+                }
+            return Game(id, grabs)
+        }
+    }
+}
+
+data class NbCube(val nb: Int, val color: CubeColor)
+
+enum class CubeColor(val max: Int) {
+    RED(12), GREEN(13), BLUE(14);
 }
--- src/test/kotlin/adventofcode2023/day2/Day2KtTest.kt
+++ src/test/kotlin/adventofcode2023/day2/Day2KtTest.kt
@@ -6,7 +6,11 @@ import org.junit.Test
 internal class Day2KtTest {
     private val program = ProgramDay2(
         brutInputs = listOf(
-            ""
+            "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+            "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+            "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+            "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
         ).map { it },
         debug = true
     )
@@ -14,7 +18,7 @@ internal class Day2KtTest {
     @Test
     fun part1() {
         assertEquals(
-            expected = "",
+            expected = "8",
             actual = program.part1()
         )
     }
@@ -22,7 +26,7 @@ internal class Day2KtTest {
     @Test
     fun part2() {
         assertEquals(
-            expected = "",
+            expected = "2286",
             actual = program.part2()
         )
     }
