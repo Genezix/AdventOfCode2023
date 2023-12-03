e05d3f4e78a163faf6bb8b55f0bb008ef7d3a942
Florian KAISER <florian.kaiser@helios.do>
2023-12-03 02:38:21 +0100

    day1

--- src/main/kotlin/adventofcode2023/day1/ProgramDay1.kt
+++ src/main/kotlin/adventofcode2023/day1/ProgramDay1.kt
@@ -3,9 +3,38 @@ package adventofcode2023.day1
 import common.Program
 
 class ProgramDay1(brutInputs: List<String>, private val debug: Boolean = false) : Program {
+
     private val inputs = brutInputs
+    private val regexNumber = "[1-9]".toRegex()
+    val regexNumberLetter = ".*(one|two|three|four|five|six|seven|eight|nine|[1-9]).*".toRegex()
+    val regexNumberLetterReversed = ".*(eno|owt|eerht|ruof|evif|xis|neves|thgie|enin|[1-9]).*".toRegex()
 
-    override fun part1(): String = ""
+    override fun part1(): String {
+        return inputs.map {
+            (it.first { regexNumber.matches(it.toString()) }.toString() +
+                    it.last { regexNumber.matches(it.toString()) }.toString()).toInt()
+        }.sum().toString()
+    }
 
-    override fun part2(): String = ""
+    override fun part2(): String {
+        return inputs.map {
+            (regexNumberLetterReversed.find(it.reversed())!!.groupValues[1].reversed()
+                .toNum() + regexNumberLetter.find(it)!!.groupValues[1].toNum()).toInt()
+        }.sum().toString()
+    }
+}
+
+fun String.toNum(): String {
+    return when (this) {
+        "one" -> "1"
+        "two" -> "2"
+        "three" -> "3"
+        "four" -> "4"
+        "five" -> "5"
+        "six" -> "6"
+        "seven" -> "7"
+        "eight" -> "8"
+        "nine" -> "9"
+        else -> this
+    }
 }
--- src/test/kotlin/adventofcode2023/day1/Day1KtTest.kt
+++ src/test/kotlin/adventofcode2023/day1/Day1KtTest.kt
@@ -6,7 +6,10 @@ import org.junit.Test
 internal class Day1KtTest {
     private val program = ProgramDay1(
         brutInputs = listOf(
-            ""
+            "1abc2",
+            "pqr3stu8vwx",
+            "a1b2c3d4e5f",
+            "treb7uchet",
         ).map { it },
         debug = true
     )
@@ -14,15 +17,43 @@ internal class Day1KtTest {
     @Test
     fun part1() {
         assertEquals(
-            expected = "",
+            expected = "142",
             actual = program.part1()
         )
     }
 
     @Test
     fun part2() {
+        val program = ProgramDay1(
+            brutInputs = listOf(
+                "two1nine",
+                "eightwothree",
+                "abcone2threexyz",
+                "xtwone3four",
+                "4nineeightseven2",
+                "zoneight234",
+                "7pqrstsixteen",
+            ).map { it },
+            debug = true
+        )
+
         assertEquals(
-            expected = "",
+            expected = "281",
+            actual = program.part2()
+        )
+    }
+
+    @Test
+    fun part3() {
+        val program = ProgramDay1(
+            brutInputs = listOf(
+                "eightwo",
+            ).map { it },
+            debug = true
+        )
+
+        assertEquals(
+            expected = "82",
             actual = program.part2()
         )
     }
