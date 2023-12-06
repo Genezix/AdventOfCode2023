8cf52a9f6427db671dfddcda480ca64e833b618d
Florian KAISER <florian.kaiser@helios.do>
2023-12-06 01:36:57 +0100

    day 5 part 1

--- src/main/kotlin/adventofcode2023/day5/ProgramDay5.kt
+++ src/main/kotlin/adventofcode2023/day5/ProgramDay5.kt
@@ -1,11 +1,146 @@
 package adventofcode2023.day5
 
 import common.Program
+import common.parseGroupsList
 
 class ProgramDay5(brutInputs: List<String>, private val debug: Boolean = false) : Program {
-    private val inputs = brutInputs
+    private val almanac = brutInputs.parseGroupsList()
 
-    override fun part1(): String = ""
+    override fun part1(): String = almanac.let { Almanac.build(it) }.lowestLocation().toString()
 
-    override fun part2(): String = ""
+    override fun part2(): String = almanac.let { Almanac.buildRange(it) }.lowestLocation().toString()
+}
+
+data class Almanac(
+    val seeds: List<LongRange>,
+    val mapping: List<ElementMapping>
+) {
+    fun lowestLocation(): Long {
+        return seeds.minOf {
+            it.minOf { mapping.findLocation(ElementType.SEED, it) }
+        }
+    }
+
+    private fun List<ElementMapping>.findLocation(source : ElementType, index: Long) : Long {
+        if(source == ElementType.LOCATION) return index
+        val element = this.first { it.source == source }
+        val newIndex = element.mappings.firstOrNull { it.sourceRange.contains(index) }?.let {
+            val delta = index - it.sourceRange.first
+            it.destinationRange.first + delta
+        } ?: index
+        return findLocation(element.destination, newIndex)
+    }
+
+    companion object {
+        fun buildRange(inputs: List<List<String>>) : Almanac {
+            //seeds: 79 14 55 13
+            val inputSeeds = inputs[0][0].split(": ")[1].split(" ").map { it.toLong() }
+            val seeds = mutableListOf<LongRange>()
+            for (i : Int in inputSeeds.indices)  {
+                if(i % 2 == 0) {
+                    val start = inputSeeds[i]
+                    val end = start + inputSeeds[i + 1] - 1
+                    seeds.add(LongRange(start, end))
+                }
+            }
+
+//            seed-to-soil map:
+//            50 98 2
+//            52 50 48
+            val elementMappings = inputs
+                .filterIndexed { index, _ -> index != 0 }
+                .map { mapping ->
+                    mapping[0]
+                        .split(" ")[0]
+                        .split("-to-")
+                        .map { ElementType.valueOf(it.uppercase()) }.let { elementsTypes ->
+                            val mappings = mapping
+                                .filterIndexed { index, _ -> index != 0 }
+                                .map {
+                                    it.split(" ").let { rangePart ->
+                                        val size = rangePart[2].toLong() - 1
+                                        val destination = rangePart[0].toLong()
+                                        val source = rangePart[1].toLong()
+                                        ElementRange(
+                                            sourceRange = LongRange(
+                                                source,
+                                                source + size
+                                            ),
+                                            destinationRange = LongRange(
+                                                destination,
+                                                destination + size
+                                            ),
+                                        )
+                                    }
+                                }
+
+                            ElementMapping(
+                                elementsTypes[0],
+                                elementsTypes[1],
+                                mappings
+                            )
+                        }
+                }
+
+            return Almanac(seeds, elementMappings)
+        }
+        fun build(inputs: List<List<String>>): Almanac {
+            //seeds: 79 14 55 13
+            val seeds = inputs[0][0].split(": ")[1].split(" ").map {
+                LongRange(it.toLong(), it.toLong())
+            }
+
+//            seed-to-soil map:
+//            50 98 2
+//            52 50 48
+            val elementMappings = inputs
+                .filterIndexed { index, _ -> index != 0 }
+                .map { mapping ->
+                    mapping[0]
+                        .split(" ")[0]
+                        .split("-to-")
+                        .map { ElementType.valueOf(it.uppercase()) }.let { elementsTypes ->
+                            val mappings = mapping
+                                .filterIndexed { index, _ -> index != 0 }
+                                .map {
+                                    it.split(" ").let { rangePart ->
+                                        val size = rangePart[2].toLong() - 1
+                                        val destination = rangePart[0].toLong()
+                                        val source = rangePart[1].toLong()
+                                        ElementRange(
+                                            sourceRange = LongRange(
+                                                source,
+                                                source + size
+                                            ),
+                                            destinationRange = LongRange(
+                                                destination,
+                                                destination + size
+                                            ),
+                                        )
+                                    }
+                                }
+
+                            ElementMapping(
+                                elementsTypes[0],
+                                elementsTypes[1],
+                                mappings
+                            )
+                        }
+                }
+
+            return Almanac(seeds, elementMappings)
+        }
+    }
+}
+
+data class ElementMapping(
+    val source: ElementType,
+    val destination: ElementType,
+    val mappings: List<ElementRange>
+)
+
+data class ElementRange(val sourceRange: LongRange, val destinationRange: LongRange)
+
+enum class ElementType {
+    SEED, SOIL, FERTILIZER, WATER, LIGHT, TEMPERATURE, HUMIDITY, LOCATION
 }
--- src/test/kotlin/adventofcode2023/day5/Day5KtTest.kt
+++ src/test/kotlin/adventofcode2023/day5/Day5KtTest.kt
@@ -6,7 +6,39 @@ import org.junit.Test
 internal class Day5KtTest {
     private val program = ProgramDay5(
         brutInputs = listOf(
-            ""
+            "seeds: 79 14 55 13",
+            "",
+            "seed-to-soil map:",
+            "50 98 2",
+            "52 50 48",
+            "",
+            "soil-to-fertilizer map:",
+            "0 15 37",
+            "37 52 2",
+            "39 0 15",
+            "",
+            "fertilizer-to-water map:",
+            "49 53 8",
+            "0 11 42",
+            "42 0 7",
+            "57 7 4",
+            "",
+            "water-to-light map:",
+            "88 18 7",
+            "18 25 70",
+            "",
+            "light-to-temperature map:",
+            "45 77 23",
+            "81 45 19",
+            "68 64 13",
+            "",
+            "temperature-to-humidity map:",
+            "0 69 1",
+            "1 0 69",
+            "",
+            "humidity-to-location map:",
+            "60 56 37",
+            "56 93 4",
         ).map { it },
         debug = true
     )
@@ -14,7 +46,7 @@ internal class Day5KtTest {
     @Test
     fun part1() {
         assertEquals(
-            expected = "",
+            expected = "35",
             actual = program.part1()
         )
     }
@@ -22,7 +54,7 @@ internal class Day5KtTest {
     @Test
     fun part2() {
         assertEquals(
-            expected = "",
+            expected = "46",
             actual = program.part2()
         )
     }
